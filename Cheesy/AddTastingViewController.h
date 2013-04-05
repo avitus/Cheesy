59590d5613b975a3132a4164375edc6867aa4e23
Andy Vitus <avitus@gmail.com>
2013-04-05 16:57:06 -0700

    Working on add view

---
+++ Cheesy/AddTastingViewController.h
@@ -0,0 +1,22 @@
+//
+//  AddTastingViewController.h
+//  Cheesy
+//
+//  Created by Andy Vitus on 4/5/13.
+//  Copyright (c) 2013 Andy Vitus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CheeseTasting;
+
+@interface AddTastingViewController : UITableViewController <UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *cheeseNameInput;
+@property (weak, nonatomic) IBOutlet UITextField *storeNameInput;
+@property (weak, nonatomic) IBOutlet UITextField *priceInput;
+@property (weak, nonatomic) IBOutlet UITextField *ratingInput;
+
+@property (strong, nonatomic) CheeseTasting *cheeseTasting;
+
+@end
